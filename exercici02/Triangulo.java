f94c24626649d27afbf578ea946ac0dea9fe166f
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2023-08-15 20:02:31 -0300

    Add files via upload

--- exercici02/Triangulo.java
+++ exercici02/Triangulo.java
@@ -10,9 +10,30 @@ package exercici02;
  * @author 20222TPMI0035
  */
 public class Triangulo {
-    public double A;
-    public double B;
-    public double C;
+    public double ladoA;
+    public double ladoB;
+    public double ladoC;
+    
+    public void Ledados(){
+        ladoA = InOut.leInt("Qual o valor do ladoA ?");
+        ladoB = InOut.leInt("Qual o valor do ladoB ?");
+        ladoC = InOut.leInt("Qual o valor do ladoC ?");
+    }
+    public double Area(){
+       double s = (ladoA+ladoB+ladoC)/2;
+       double calculoArea = s * (s-ladoA) * (s-ladoB)* (s-ladoC);
+       double area = calculoArea 
+        
+    }
+    
+    public boolean verificaTriangulo(){
+        if (ladoA < ladoB+ladoC && ladoC < ladoB+ladoA && ladoB < ladoA+ladoC) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
     
     
